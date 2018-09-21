55418b46131e64d7f279818f75f431ddf64266cf
Gabriel Sussumu Kato <gabrielsussumu@outlook.com>
2018-09-21 00:21:18 -0300

    Create all planned sections, fun with viewport units!

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,46 @@
 <template>
   <div id="app">
+    <section id="landing-section"></section>
+		<section id="intro"></section>
+		<section id="competencies"></section>
+		<section id="photos"></section>
+		<section id="contact-form"></section>
   </div>
 </template>
+
+<style lang="scss" scoped>
+#app {
+	width: 100%;
+	height: 100%;
+}
+
+#landing-section {
+	background-color: #5ad1ad;
+	width: 100%;
+	height: 100%;
+}
+
+#intro {
+	background-color: #5aafd1;
+	width: 100vw;
+	height: 100vh;
+}
+
+#competencies {
+	background-color: #625ad1;
+	width: 100vw;
+	height: 100vh;
+}
+
+#photos {
+	background-color: #d15a96;
+	width: 100vw;
+	height: 100vh;
+}
+
+#contact-form {
+	background-color: #d1cf5a;
+	width: 100vw;
+	height: 100vh;
+}
+</style>
