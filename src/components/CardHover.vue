6ff72b3d4bd0ffa9130c20d455c7fb3f316c3b5f
Gabriel Sussumu Kato <gabrielsussumu@outlook.com>
2018-09-27 22:59:01 -0300

    Add card component that shows an image passed by prop and a message on mouse hover

---
+++ src/components/CardHover.vue
@@ -0,0 +1,44 @@
+<template>
+	<div class="card" @mouseover="showCardMessage = true" @mouseout="showCardMessage = false">
+		<div class="card-front" v-if="!showCardMessage">
+			<img :src="imgSrc" />
+		</div>
+		<div class="card-front" v-else>
+			<img :src="imgSrc" />
+			<span>{{message}}</span>
+		</div>
+	</div>	
+</template>
+
+<script>
+export default {
+	props: {
+		imgSrc: {
+			type: String,
+			default: ""
+		},
+		message: {
+			type: String,
+			default: "default message :("
+		}
+	},
+	data () {
+		return {
+			showCardMessage: false
+		}
+	}
+}
+</script>
+
+<style lang="scss" scoped>
+.card {
+	width: 150px;
+	height: 250px;
+}
+
+.card .card-front {
+	width: 100%;
+	height: 100%;
+}
+</style>
+
