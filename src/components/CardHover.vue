19556fee6360409f66acfb6cbec76998c773b50c
Gabriel Sussumu Kato <gabrielsussumu@outlook.com>
2018-10-02 23:05:02 -0300

    Card flip using just css from https://davidwalsh.name/css-flip

--- src/components/CardHover.vue
+++ src/components/CardHover.vue
@@ -1,13 +1,15 @@
 <template>
-	<div class="card-hover" @mouseover="showCardMessage = true" @mouseout="showCardMessage = false">
-		<div class="card-front" v-if="!showCardMessage">
-			<img :src="imgSrc" />
+	<div class="card-container">
+		<div class="card-hover">
+			<div class="card-front">
+				<img :src="imgSrc" />
+			</div>
+			<div class="card-back">
+				<img :src="imgSrc" />
+				<span>{{message}}</span>
+			</div>
 		</div>
-		<div class="card-back" v-else>
-			<img :src="imgSrc" />
-			<span>{{message}}</span>
-		</div>
-	</div>	
+	</div>
 </template>
 
 <script>
@@ -31,12 +33,45 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.card-front, .card-back {
-	max-width: 300px;
+.card-container {
+	perspective: 1000px;
 }
 
-.card-hover img {
-	max-height: 100%;
-  max-width: 100%;
+.card-container:hover {
+	transform: rotateY(180deg);
 }
+
+.card-container,
+.card-front,
+.card-back {
+	width: 200px;
+	height: 200px;
+}
+
+.card-hover {
+	transition: 0.5s;
+	transform-style: preserve-3d;
+	position: relative;
+}
+
+.card-front,
+.card-back {
+	backface-visibility: hidden;
+	position: absolute;
+	top: 0;
+	left: 0;
+}
+
+.card-front {
+	z-index: 2;
+}
+
+.card-back {
+	transform: rotateY(180deg);
+}
+
+// .card-hover img {
+// 	max-height: 100%;
+// 	max-width: 100%;
+// }
 </style>
