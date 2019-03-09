f0e2bf3cf57e533d0da4c110db900ac21448d570
Gabriel Sussumu Kato <gkato@braspag.com.br>
2019-03-09 15:47:44 -0300

    Big cleanup on website

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,20 @@
 <template>
   <div id="app">
     <section id="landing-section" class="fill-screen">
-			<div id="title">GABRIEL KATO</div>
-			<div @click="scrollToSection('intro')">
-				<img class="snap-scroll" src="./assets/down-arrow.png" alt="myself giving a talk about GraphQL" />
-			</div>
-		</section>
-		<section id="intro" class="fill-half-screen" ref="intro">
-			<div id="my-photo">
-				<img src="./assets/myself.jpg" alt="a photo showing my face" />
-			</div>
-			<div id="about-me">
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sodales, neque nec vulputate posuere, velit sem sagittis sapien, sit amet pellentesque elit metus nec elit. Donec vel dolor mauris. Curabitur interdum, neque ac pellentesque convallis, sapien tellus mollis erat, et consectetur velit justo nec ante. Mauris maximus euismod lorem, sed egestas urna ullamcorper in. Etiam ut purus elementum, dictum elit ut, eleifend nisi. Fusce euismod neque pretium, dictum nibh at, iaculis urna. Phasellus sagittis lectus ac ex ultricies hendrerit. Duis consequat ligula ac enim malesuada laoreet.
-			</div>
-		</section>
-		<section id="competencies">
-			<card-hover :img-src="vueImage" message="Vue.JS" />
-			<card-hover :img-src="dotNetCoreImage" message=".NET Core" />		
-			<card-hover :img-src="sqlServerImage" message="SQL Server" />		
-		</section>
-		<section id="photos" class="fill-screen"></section>
-		<section id="contact-form" class="fill-screen"></section>
+		<div id="my-photo">
+			<img src="./assets/myself.jpg" alt="a photo showing my face" />
+		</div>
+		<div id="title">GABRIEL KATO</div>		
+		<div @click="scrollToSection('contact-form')">
+			<img class="snap-scroll" src="./assets/down-arrow.png" />
+		</div>	
+	</section>		
+	<section id="contact-form" class="fill-screen" ref="contact-form"></section>
   </div>
 </template>
 
 <script>
-import CardHover from "./components/CardHover.vue";
-import VueImage from "./assets/vue-card-image.jpg";
-import DotNetCoreImage from "./assets/net-core-card-image.png";
-import SqlServerImage from "./assets/sql-server-logo.png";
-
 export default {
-	data() {
-		return {
-			vueImage: VueImage,
-			dotNetCoreImage: DotNetCoreImage,
-			sqlServerImage: SqlServerImage
-		};
-	},
-	components: {
-		CardHover
-	},
 	methods: {
 		scrollToSection(section) {
 			this.$refs[section].scrollIntoView({
@@ -70,11 +44,12 @@ export default {
 	color: white;
 }
 
-.snap-scroll {
-	margin: 0 auto;
-	position: absolute;
-	bottom: 0;
-	left: 50%;
+#landing-section #my-photo img {
+	max-width: 30vw;
+	max-height: 30vh;
+	border-radius: 140px;
+	display: block;
+	margin: auto;
 }
 
 #landing-section {
@@ -85,16 +60,23 @@ export default {
 	align-items: center;
 }
 
-#intro {
+#landing-section {
 	display: flex;
 	justify-content: space-around;
 	// fl	ex-wrap: wrap;
 }
 
-#intro div {
+#landing-section div {
 	margin: auto;
 	padding: 40px;
 	width: 50%;
+}
+
+.snap-scroll {
+	margin: 0 auto;
+	position: absolute;
+	bottom: 0;
+	left: 50%;
 }
 
 #intro #my-photo img {
@@ -105,45 +87,7 @@ export default {
 	margin: auto;
 }
 
-#intro #about-me {
-	font-family: $default-fonts;
-}
-
-#competencies {
-	background-image: linear-gradient(
-		to right top,
-		#9ef2c2,
-		#21e8ea,
-		#00d5ff,
-		#77b4ff,
-		#de84f0
-	);
-	display: flex;
-	justify-content: space-around;
-	align-items: center;
-	flex-wrap: wrap;
-}
-
-#competencies .card-container {
-	margin: 60px 0;
-}
-
-#photos {
-	background-color: #d15a96;
-}
-
 #contact-form {
 	background-color: #d1cf5a;
-}
-
-// general
-.fill-screen {
-	min-width: 100%;
-	min-height: 100vh;
-}
-
-.fill-half-screen {
-	min-width: 100%;
-	min-height: 50vh;
 }
 </style>
