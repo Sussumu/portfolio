5c9cb1da9f2223342c02188ce3ea02e2bc99bcce
Gabriel Kato <gabrielsussumu@outlook.com>
2019-12-07 21:44:12 -0300

    Gives a new face to the contact form

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -1,24 +1,65 @@
 <template>
-	<form name="contact" data-netlify="true">
-		<p>
-			<label>Name: <input type="text" name="name"/></label>
-		</p>
-		<p>
-			<label>Email: <input type="text" name="email"/></label>
-		</p>
-		<p>
-			<label>Subject: <input type="text" name="subject"/></label>
-		</p>
-		<p>
-			<label>Message: <textarea name="message"></textarea></label>
-		</p>
+    <span>
+        <span id="form-title">{{title}}</span>
+        <form id="netlify-form" name="contact" data-netlify="true">
+            <p>
+                <label>Nome: <input type="text" name="name"/></label>
+            </p>
+            <p>
+                <label>Email: <input type="text" name="email"/></label>
+            </p>
+            <p>
+                <label>Assunto: <input type="text" name="subject"/></label>
+            </p>
+            <p>
+                <label>Mensagem: <textarea name="message" rows=10></textarea></label>
+            </p>
 
-		<p><button type="submit">Send</button></p>
-	</form>
+            <p><button type="submit">Enviar!</button></p>
+        </form>
+    </span>
 </template>
 
 <script>
-export default {};
+export default {
+    props: {
+        title: {
+            default: "Contato",
+            type: String,
+            required: true
+        }
+    }
+};
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+@import "../styles/main.scss";
+
+#form-title,
+form {
+    font-family: $default-fonts;
+}
+
+#form-title {
+    font-size: 46px;
+    text-align: center;
+}
+
+#netlify-form {
+    display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+}
+
+#netlify-form p {
+    width: 30vw;
+}
+
+#netlify-form label,
+#netlify-form input,
+#netlify-form textarea {
+    display: block;
+    width: 100%;
+}
+</style>
