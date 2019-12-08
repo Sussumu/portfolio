ece2419be6762c2daee1779946237c4dc3c2a5cf
Gabriel Kato <gabrielsussumu@outlook.com>
2019-12-07 21:10:56 -0300

    Adds the contact form via netlify

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,12 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  extends: ["plugin:vue/essential", "@vue/prettier"],
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
-  },
-  parserOptions: {
-  }
+	root: true,
+	env: {
+		node: true
+	},
+	extends: ["plugin:vue/essential", "@vue/prettier"],
+	rules: {
+		"no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+		"no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+	},
+	parserOptions: {}
 };
