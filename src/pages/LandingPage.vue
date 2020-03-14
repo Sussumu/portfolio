9fed7d2f85810bb3a2a5b1a926f0df1871d3daf7
Gabriel Sussumu Kato <gkato@meuacesso.com.br>
2020-03-14 18:23:16 -0300

    Removes unused css rule

--- src/pages/LandingPage.vue
+++ src/pages/LandingPage.vue
@@ -76,17 +76,6 @@ export default {
 	text-align: center;
 }
 
-#landing-section #snap-scroll-wrapper {
-	position: absolute;
-	width: 50px;
-	height: 50px;
-	left: 0;
-	right: 0;
-	bottom: 0;
-	margin: 0 auto;
-	cursor: pointer;
-}
-
 #landing-section #page-links {
 	margin-top: 40px;
 }
