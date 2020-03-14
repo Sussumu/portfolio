42421bc6bb19c7afc6e925817f5ed7f3bcbf73e6
Gabriel Sussumu Kato <gkato@meuacesso.com.br>
2020-03-14 18:27:48 -0300

    Moves back image section to keep the same background in all pages

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,25 @@
 <template>
 	<div id="app">
-		<router-view></router-view>		
+		<section id="landing-section" class="fill-screen">
+			<router-view></router-view>	
+		</section>	
 	</div>
 </template>
+
+<style lang="scss">
+@import "./styles/main.scss";
+
+#landing-section {
+	background: url("./assets/landing-image.jpg") no-repeat center center fixed;
+	background-size: cover;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+}
+
+#landing-section div {
+	margin: 10px auto;
+	width: 100%;
+}
+</style>
