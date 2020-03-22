418944f4f3df8a5feb3aaee910ad914443104b6c
sussumu <gabrielsussumu@outlook.com>
2020-03-22 14:38:15 -0300

    Finishes post

--- src/pages/posts/AboutBlog.vue
+++ src/pages/posts/AboutBlog.vue
@@ -17,6 +17,25 @@
 				gerenciar um blog, mas como já não mexo com Vue.js há mais de seis meses, pensei em testar meus conhecimentos e ver se conseguia 
 				criar tudo do zero. Não sou nenhum doido de open source, mas também gosto de ter algumas coisas no meu controle, então Medium também 
 				não era uma opção.</p>
+
+				<p>O estranho é usar um componente para cada postagem, mas cada doido com suas doideras!</p>
+
+				<p>Sempre quis ter um lugar para jogar uns textos sem compromisso. Quando era adolescente cheguei a criar algumas coisas no Blogger, 
+				mas nunca divulguei para ninguém, então acho que não conta. Quando anunciaram os domínios .dev, a vontade voltou. Com ela, o mesmo 
+				<a href="https://en.wikipedia.org/wiki/Fear_of_missing_out">FOMO</a> de não conseguir um bom email @outlook. Na época eu poderia 
+				ter registrado só com o meu sobrenome Kato, mas perdi a chance! Depois de conseguir o meu domínio, ainda se passaram meses até que 
+				eu resolvesse criar o site... Então por R$ 50,00 por ano eu mantenho o meu .dev. Ou como gostam de colocar, um cafezinho por mês.</p>
+
+				<p>Falando sobre stack, o Netlify me ajudou muito nessa jornada. Poder hospedar um site estático sem custo e cheio de features é 
+				muito bom. Outra solução acho que seria um Github Pages, mas da outra vez que tentei não fui muito bem sucedido. Como eles mesmo 
+				anunciam, basta dizer onde está o seu repositório, configurar a ferramenta de build e só! Além disso também tem suporte a formulários 
+				(sim, no site estático!), autenticação e serverless functions de graça. Caso precise escalar esses serviços, aí sim o preço começa a 
+				subir, mas para um site pessoal com links e alguns textos como o meu não faz diferença.</p>
+
+				<p>E como o site é estático, tudo é feito com Vue.js, meu preferido. De fato, não tenho muita bagagem de frameworks de frontend para 
+				comparar. Entre Vue e Angular 1 não é muito difícil de escolher!</p>
+
+				<p>Por enquanto é só isso pra falar!</p>
 			</template>
 		</blog-post>
 	</div>
