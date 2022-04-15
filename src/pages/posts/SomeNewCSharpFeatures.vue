21c2b1eff15265cdfde8dbf362019f98f58a51b0
Gabriel Sussumu Kato <gabrielsussumu@outlook.com>
2022-04-15 18:13:26 -0300

    Lint and add the whole assets folder as lfs

--- src/pages/posts/SomeNewCSharpFeatures.vue
+++ src/pages/posts/SomeNewCSharpFeatures.vue
@@ -9,16 +9,16 @@
 			</template>
 			<template v-slot:content>
 				<p>
-					With the announcement of the latest C# version 9.0 I started thinking about how much content 
-					I've been missing in the last years. The last big thing I heard about was tuples and that span 
-					thing. But to be honest, I never tried those! As we all hopefully know by now, it's impossible 
-					to keep up with everything that is going on. That being said, it's good to know most language 
-					features to be able to solve a problem in a better way.
+					With the announcement of the latest C# version 9.0 I started thinking
+					about how much content I've been missing in the last years. The last
+					big thing I heard about was tuples and that span thing. But to be
+					honest, I never tried those! As we all hopefully know by now, it's
+					impossible to keep up with everything that is going on. That being
+					said, it's good to know most language features to be able to solve a
+					problem in a better way.
 				</p>
 
-				<p> 
-					
-				</p>
+				<p></p>
 			</template>
 		</blog-post>
 	</div>
